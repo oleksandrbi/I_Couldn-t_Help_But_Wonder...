fac92284bccf5daffbaa307272edf22e9c9515fd
oleksandrbi <42999823+oleksandrbi@users.noreply.github.com>
2019-11-05 21:35:08 -0500

    Add files via upload

--- getData.py
+++ getData.py
@@ -2,21 +2,14 @@
 #Using Zomato API we obtain resturants names in the Las Vegas area and their Reviews
 #API KEY: aa2e170fced5e4de42b96789a76fbd7f
 
-import zomatopy
+import requests
+from pprint import pprint
 
-config = { "user_key":"aa2e170fced5e4de42b96789a76fbd7f" }
-city_name = "Las Vegas"
 
-zomato = zomatopy.initialize_app(config)
+locationUrlFromLatLong = "https://developers.zomato.com/api/v2.1/cities?lat=28&lon=77"
+header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "aa2e170fced5e4de42b96789a76fbd7f"}
 
-#Getting ID for a particular city
-city_ID = zomato.get_city_ID(city_name)
+response = requests.get(locationUrlFromLatLong, headers=header)
 
-#Getting the cuisines in a city
-cuisine_dictionary = get_cuisines(city_ID)
+pprint(response.json())
 
-#Getting all establishment types in a city.
-establishment_types_dictionary = get_establishment_types(city_ID)
-
-#Getting the details of a particular restaurant
-restaurant_details = get_restaurant(restaurant_ID)
