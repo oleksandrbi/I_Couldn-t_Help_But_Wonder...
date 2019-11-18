366fde64765ebf0b5cf9ff738708155499d747c3
houseofinvention <tonyxueyuliang@gmail.com>
2019-11-18 17:40:09 -0500

    server and client update

--- client.py
+++ client.py
@@ -1,23 +1,31 @@
 import socket
+import select
 
 HOST = '127.0.0.1'
 PORT = 65432
 
+def ui(s, label):
+    if label == 'y':
+        print('sending y')
+        s.send(b'y')
+    elif label == 'n':
+        print('sending n')
+        s.send(b'n')
+    elif label == 'exit':
+        running = False
+    else:
+        print("Wrong format of input, Please enter y for yes, n for no. exit for closing this program")
+        rl = input()
+        ui(s, rl)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.send(b'yx')
+    s.settimeout(5)
 
     running = True
     while(running):
         data = s.recv(1024)
         print('Received', repr(data))
         label = input()
-        if label == 'y':
-            print('sending y')
-            s.send(b'y')
-        elif label == 'n':
-            print('sending n')
-            s.send(b'n')
-        elif label == 'exit':
-            running = False
-        else: print("Wrong format of input")
+        ui(s, label)
--- server.py
+++ server.py
@@ -1,26 +1,44 @@
-import socket
+import socket, threading
 import sys
-
-HOST = '127.0.0.1'
-PORT = 65432
 
 corpus = ['Comment 1', 'Comment 2', 'Comment 3', 'Comment 4']
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
+#Thread to handle socket connection from client
+class ClientThread(threading.Thread):
+    def __init__(self,clientAddress,clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print ("New connection from: ", clientAddress)
+    def run(self):
+        print ("Connection from : ", clientAddress)
+        msg = ''
         while True:
-            data = conn.recv(1024)
+            data = self.csocket.recv(1024)
             if not data:
                 break
             datas = data.decode('ASCII')
             if datas == 'yx':
                 for x in corpus:
                     dx = x.encode('ASCII')
-                    conn.send(dx)
-                    label = conn.recv(1024)
+                    self.csocket.send(dx)
+                    label = self.csocket.recv(1024)
                     print(label)
+
+                    #store label into database
                     sys.stdout.flush()
+        print ("Client at ", clientAddress , " disconnected...")
+
+LOCALHOST = "127.0.0.1"
+PORT = 65432
+
+#declaring server socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
