6f960df222f44e5eecffb41ab86b405012d453d7
oleksandrbi <42999823+oleksandrbi@users.noreply.github.com>
2019-11-25 21:27:55 -0500

    Add files via upload
    vegas dataset updates

--- yelpDATASET/yelpFilter.py
+++ yelpDATASET/yelpFilter.py
@@ -7,13 +7,15 @@
 import json
 import pandas as pd
 
-filePATH = 'C:\\Users\\obiha\\Desktop\\yelp_dataset\\business.json'
+BUZZfilePATH = 'C:\\Users\\obiha\\Desktop\\yelp_dataset\\business.json'
+REVfilePATH = 'C:\\Users\\obiha\\Desktop\\yelp_dataset\\reviews.json'
 
+#ALL STUFF TO GET VEGAS BUSINESS 
 # open input file: 
-ifile = open(filePATH, encoding="utf8") 
+Bfile = open(BUZZfilePATH, encoding="utf8") 
 stop = 192574
 all_data = list()
-for i, line in enumerate(ifile):  
+for i, line in enumerate(Bfile):  
     # convert the json on this line to a dict
     data = json.loads(line)
     # extract what we want
@@ -38,6 +40,42 @@ for i, line in enumerate(ifile):
 
 # create the DataFrame
 df = pd.DataFrame(all_data, columns=['business_id','name','address','city','state','postal_code','latitude','longitude','stars','review_count','is_open','attributes','categories','hours'])
-df.to_csv('vegas.csv')
+df.to_csv('vegasBUZZ.csv')
+print("BUSINESS")
 print(df)
-ifile.close()
+Bfile.close()
+
+#ALL STUFF TO GET VEGAS REVIEWS
+# open input file: 
+Rfile = open(BUZZfilePATH, encoding="utf8") 
+stop = 192574
+all_data = list()
+for i, line in enumerate(Rfile):  
+    # convert the json on this line to a dict
+    data = json.loads(line)
+    # extract what we want
+    business_id = data['business_id']
+    name = data['name']
+    address = data['address']
+    city = data['city']
+    state = data['state']
+    postal_code = data['postal_code']
+    latitude = data['latitude']
+    longitude = data['longitude']
+    stars = data['stars']
+    review_count = data['review_count']
+    is_open = data['is_open']
+    attributes = data['attributes']
+    categories = data['categories']
+    hours = data['hours']
+
+    # add to the data collected so far
+    if (city == "Las Vegas"):
+        all_data.append([business_id,name,address,city,state,postal_code,latitude,longitude,stars,review_count,is_open,attributes,categories,hours])
+
+# create the DataFrame
+df = pd.DataFrame(all_data, columns=['business_id','name','address','city','state','postal_code','latitude','longitude','stars','review_count','is_open','attributes','categories','hours'])
+df.to_csv('vegasREV.csv')
+print("REVIEWS")
+print(df)
+Rfile.close()
